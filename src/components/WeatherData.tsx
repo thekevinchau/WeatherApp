8a1af67b0f57de1fff7efda4236fcc4ba51bea49
KEVIN CHAU <kevinch@udel.edu>
2024-03-22 03:17:20 -0400

    Added text and state for each relevant property

--- src/components/WeatherData.tsx
+++ src/components/WeatherData.tsx
@@ -1,23 +1,46 @@
 import { useEffect, useState } from "react";
-import { SearchBar } from "./SearchBar";
-
 
 export function WeatherData(){
-    const [weather, setWeather] = useState<number>(0);
+    const [country, setCountry] = useState<string>("");
+    const [city, setCity] = useState<string>("");
+    const [image, setImage] = useState<string>("");
+    const [temp, setTemp] = useState<number>(0);
+    const [humidity, setHumidity] = useState<number>(0);
+    const [wind, setWind] = useState<number>(0);
+    const [windDir, setWindDir] = useState<string>("");
+
+    function retrieveURL(country: string){
+        return `https://api.weatherapi.com/v1/current.json?key=2aba150aac6549a981514758242902&q=${country}`
+    }
+
+
     useEffect(() => {
         const fetchData = async () => {
-          const fetchData = await fetch("https://api.weatherapi.com/v1/current.json?key=2aba150aac6549a981514758242902&q=afghanistan")
+            const url = retrieveURL("pakistan")
+          const fetchData = await fetch(url);
           const response = await fetchData.json();
           console.log(response);
-          setWeather(response.current.humidity)
+          setImage(response.current.condition.icon);
+          setTemp(response.current.temp_f)
+          setCountry(response.location.country);
+          setCity(response.location.name);
+          setHumidity(response.current.humidity);
+          setWind(response.current.wind_mph);
+          setWindDir(response.current.wind_dir)
         };
         fetchData();
        }, [])
       
     return (
-        <div className="">
-            <SearchBar></SearchBar>
-            <h1>Humidity :{weather}</h1>
+        <div>
+            <img src={image}></img>
+            <p>{`Current Temperature: ${temp}`}</p>
+            <p>{`Humidity: ${humidity}`}</p>
+            <div>
+            <p>{`Wind Speed: ${wind}`}</p>
+            <p>{`Wind Direction: ${windDir}`}</p>
+            </div>
+            <h3>{`${city},${country}`}</h3>
         </div>
     )
 }
