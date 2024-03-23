6bbcc7e941e07af19013548e8b49de934c14d2a1
Kevin Chau <Kevin Chau@DESKTOP-TA19N7F>
2024-03-23 00:28:27 -0400

    added units to app

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,8 +1,18 @@
 import React from "react"
 
-export function SearchBar(){
+interface searchBarProps{
+    setLocation: (location: string) => void;
+}
+
+export function SearchBar({setLocation}: searchBarProps){
+
+    function handleInput(event: React.ChangeEvent<HTMLInputElement>){
+        setLocation(event.target.value);
+    }
+
+
     return <div>
-        <input type="text" placeholder="Country">
+        <input type="text" placeholder="Country" onChange={handleInput}>
         </input>
     </div>
 }
--- src/components/WeatherData.tsx
+++ src/components/WeatherData.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { SearchBar } from "./SearchBar";
 
 export function WeatherData(){
     const [country, setCountry] = useState<string>("");
@@ -8,6 +9,8 @@ export function WeatherData(){
     const [humidity, setHumidity] = useState<number>(0);
     const [wind, setWind] = useState<number>(0);
     const [windDir, setWindDir] = useState<string>("");
+    const [region, setRegion] = useState<string>("");
+    const [location, setLocation] = useState<string>("philadelphia");
 
     function retrieveURL(location: string){
         return `https://api.weatherapi.com/v1/current.json?key=2aba150aac6549a981514758242902&q=${location}`
@@ -17,7 +20,7 @@ export function WeatherData(){
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const url = retrieveURL("detroit")
+                const url = retrieveURL(location)
                 const fetchData = await fetch(url);
                 const response = await fetchData.json();
                 console.log(response);
@@ -28,6 +31,7 @@ export function WeatherData(){
                 setHumidity(response.current.humidity);
                 setWind(response.current.wind_mph);
                 setWindDir(response.current.wind_dir)
+                setRegion(response.location.region)
             }
             catch{
                 console.log("Error receiving data!");
@@ -40,13 +44,14 @@ export function WeatherData(){
     return (
         <div className="flex flex-col items-center">
             <img src={image}></img>
-            <p>{`Current Temperature: ${temp}`}</p>
-            <p>{`Humidity: ${humidity}`}</p>
+            <p>{`Current Temperature: ${temp}°F`}</p>
+            <p>{`Humidity: ${humidity}%`}</p>
             <div>
-            <p>{`Wind Speed: ${wind}`}</p>
+            <p>{`Wind Speed: ${wind} mph`}</p>
             <p>{`Wind Direction: ${windDir}`}</p>
             </div>
-            <h3>{`${city},${country}`}</h3>
+            <h3>{`${city}, ${region}, ${country}`}</h3>
+            <SearchBar setLocation={setLocation}></SearchBar>
         </div>
     )
 }
