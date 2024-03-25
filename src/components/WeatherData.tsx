8961e1962f9af92ced6826c2dd776dd7ee0c70d9
Kevin Chau <Kevin Chau@DESKTOP-TA19N7F>
2024-03-25 10:50:01 -0400

    Added ability to query the search bar using key words

--- src/components/WeatherData.tsx
+++ src/components/WeatherData.tsx
@@ -10,34 +10,34 @@ export function WeatherData(){
     const [wind, setWind] = useState<number>(0);
     const [windDir, setWindDir] = useState<string>("");
     const [region, setRegion] = useState<string>("");
-    const [location, setLocation] = useState<string>("philadelphia");
+    const [location, setLocation] = useState<string>("Cupertino");
 
     function retrieveURL(location: string){
         return `https://api.weatherapi.com/v1/current.json?key=2aba150aac6549a981514758242902&q=${location}`
     }
 
+    async function fetchData() {
+        try{
+            const url = retrieveURL(location);
+            const fetchedData = await fetch (url);
+            const response = await fetchedData.json();
+            console.log(response);
+            setImage(response.current.condition.icon);
+            setTemp(response.current.temp_f)
+            setCountry(response.location.country);
+            setCity(response.location.name);
+            setHumidity(response.current.humidity);
+            setWind(response.current.wind_mph);
+            setWindDir(response.current.wind_dir)
+            setRegion(response.location.region)
+        }
+        catch{
+            console.log("Error receiving data!");
+        }
+    }
+
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const url = retrieveURL(location)
-                const fetchData = await fetch(url);
-                const response = await fetchData.json();
-                console.log(response);
-                setImage(response.current.condition.icon);
-                setTemp(response.current.temp_f)
-                setCountry(response.location.country);
-                setCity(response.location.name);
-                setHumidity(response.current.humidity);
-                setWind(response.current.wind_mph);
-                setWindDir(response.current.wind_dir)
-                setRegion(response.location.region)
-            }
-            catch{
-                console.log("Error receiving data!");
-            }
-        
-        };
         fetchData();
        }, [])
       
@@ -50,8 +50,9 @@ export function WeatherData(){
             <p>{`Wind Speed: ${wind} mph`}</p>
             <p>{`Wind Direction: ${windDir}`}</p>
             </div>
-            <h3>{`${city}, ${region}, ${country}`}</h3>
+            {region !== '' ? <h3>{`${city}, ${region}, ${country}`}</h3> : <h3>{`${city},${country}`}</h3>}
             <SearchBar setLocation={setLocation}></SearchBar>
+            <button onClick={fetchData}>Fetch Data</button>
         </div>
     )
 }
