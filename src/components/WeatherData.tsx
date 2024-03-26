e7f82b8e88aeb06b52185ea660ff139d68c59ae4
Kevin Chau <Kevin Chau@DESKTOP-TA19N7F>
2024-03-26 12:29:49 -0400

    Adding first steps of date parsing to functions

--- src/components/WeatherData.tsx
+++ src/components/WeatherData.tsx
@@ -2,23 +2,48 @@ import { useEffect, useState } from "react";
 import { SearchBar } from "./SearchBar";
 
 export function WeatherData(){
+    //Geographical location related states
     const [country, setCountry] = useState<string>("");
+    const [location, setLocation] = useState<string>("Cupertino");
     const [city, setCity] = useState<string>("");
+    const [region, setRegion] = useState<string>("");
+
+
+    //Weather condition related states
     const [image, setImage] = useState<string>("");
     const [temp, setTemp] = useState<number>(0);
     const [humidity, setHumidity] = useState<number>(0);
     const [wind, setWind] = useState<number>(0);
     const [windDir, setWindDir] = useState<string>("");
-    const [region, setRegion] = useState<string>("");
-    const [location, setLocation] = useState<string>("Cupertino");
 
-    function retrieveURL(location: string){
+    //Time related states
+    const [retrievedTime, setRetrievedTime] = useState<string>("");
+    const [date, setDate] = useState<string>("");
+
+
+    //function to split and parse the time into clock time and date
+    async function splitRetrievedTime(timeData: string){
+        const splitTimeData = await timeData.split(" ");
+
+        //Inner asynchronous function to split the date and reconstructs it based on MM/DD/YYYY format
+        async function splitDate(date: string){            
+            const dateObject = new Date(date);
+            const month = dateObject.toLocaleString('default', {month: 'long'});
+
+            console.log(month);
+        }
+        splitDate(splitTimeData[0]);
+    }
+    splitRetrievedTime(retrievedTime);
+
+    //Queries the API for the particular location entered into the searchbar
+    function retrieveQuery(location: string){
         return `https://api.weatherapi.com/v1/current.json?key=2aba150aac6549a981514758242902&q=${location}`
     }
 
     async function fetchData() {
         try{
-            const url = retrieveURL(location);
+            const url = retrieveQuery(location);
             const fetchedData = await fetch (url);
             const response = await fetchedData.json();
             console.log(response);
@@ -30,6 +55,7 @@ export function WeatherData(){
             setWind(response.current.wind_mph);
             setWindDir(response.current.wind_dir)
             setRegion(response.location.region)
+            setRetrievedTime(response.location.localtime)
         }
         catch{
             console.log("Error receiving data!");
@@ -40,9 +66,11 @@ export function WeatherData(){
     useEffect(() => {
         fetchData();
        }, [])
+
       
     return (
         <div className="flex flex-col items-center">
+            <p>{date}</p>
             <img src={image}></img>
             <p>{`Current Temperature: ${temp}°F`}</p>
             <p>{`Humidity: ${humidity}%`}</p>
